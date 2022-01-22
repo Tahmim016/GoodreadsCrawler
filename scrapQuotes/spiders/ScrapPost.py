6c04b84700b2eb1e4ab2822343b6cc30159115fa
Tahmim <94103583+Tahmim016@users.noreply.github.com>
2022-01-22 14:44:33 +0600

    Update ScrapPost.py

--- scrapQuotes/spiders/ScrapPost.py
+++ scrapQuotes/spiders/ScrapPost.py
@@ -1,24 +1,27 @@
 import scrapy
 
 class ScrapeQuotes(scrapy.Spider):
-    name = 'quotes'
+    #class name
+    name = 'quotes'    
 
     start_urls = [
         'https://www.goodreads.com/quotes/'
     ]
-
+    
     def parse(self, response):
-        #selector = response.css('.quote')
+        #extracting details
         for item in response.css('.quote'):
 
             quote = item.css('.quoteText::text').get()
             author = item.css('.authorOrTitle::text').get()
-
+        
+            #yielding the extracted details creating a dict
             yield {
                 'Quote': quote,
                 'Author':author
             }
 
+        #parsing next pages
         next_page = response.css('.next_page::attr(href)').get()
         if next_page is not None:
             next_page = response.urljoin(next_page)
