bdda8f28c999bb54a6baddd3bd1ed7937f5fdf9b
Tahmim <94103583+Tahmim016@users.noreply.github.com>
2022-01-22 14:24:27 +0600

    Add files via upload

---
+++ scrapQuotes/spiders/ScrapPost.py
@@ -0,0 +1,25 @@
+import scrapy
+
+class ScrapeQuotes(scrapy.Spider):
+    name = 'quotes'
+
+    start_urls = [
+        'https://www.goodreads.com/quotes/'
+    ]
+
+    def parse(self, response):
+        #selector = response.css('.quote')
+        for item in response.css('.quote'):
+
+            quote = item.css('.quoteText::text').get()
+            author = item.css('.authorOrTitle::text').get()
+
+            yield {
+                'Quote': quote,
+                'Author':author
+            }
+
+        next_page = response.css('.next_page::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page,callback=self.parse)
